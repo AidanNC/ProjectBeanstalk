68200c3048471128b7e3920dadacd0b8132de217
Aidan Nuzum-Clark <aidan@Aidans-Laptop.local>
2025-07-20 13:38:35 -0400

    finish merge

---
+++ projectbeanstalk/scripts/bird.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var speed: float = 4.0
+@export var radius: float = 5.0
+
+var centerX = -1
+var centerZ = -1
+var circum = 2 * PI * radius
+var theta = 0
+func _physics_process(delta: float) -> void:
+	#the bird will fly clockwise in a circle using the speed and radius provided
+	#assume the bird is starting at 12 oclock
+	var distance = speed * delta
+	theta += (distance / circum) * 2 * PI
+	theta = fmod(theta, (2 * PI)) 
+	var x = radius * cos(theta) + centerX
+	var z = radius * sin(theta) + centerZ
+	global_position.x = x
+	global_position.z = z
+	#transform.basis = transform.basis.rotated(Vector3(0,1,0), theta)
+	rotation = Vector3(0, -theta + (PI/2), 0)
+	
+	
+	
+func _ready() -> void:
+	$BeanstalkBird/AnimationPlayer.play('ArmatureAction')
+	centerX = global_position.x - radius
+	centerZ = global_position.z
+	
+	
